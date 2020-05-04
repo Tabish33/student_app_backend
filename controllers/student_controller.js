781f90655a123a2cbddebbfbf1bfac2775d184a3
Tabish Bashir <tabishbashir@Tabishs-MacBook-Pro.local>
2020-05-04 20:27:47 +0530

    implement showTopTenStudentsInSubjectAcrossAllSchools and showTopTenStudentsInSubjectAcrossSchool

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -18,7 +18,7 @@ exports.addStudent = (req,res)=>{
 
 exports.showTopTenStudentsAcrossAllSchools = (req,res)=>{
     var result_type = req.body.result_type 
-    
+
     Student.aggregate([
        {
            $project: {
@@ -102,6 +102,149 @@ exports.showTopTenStudentsAcrossSchool = (req,res)=>{
        {
             $limit : 10  
        } 
+    ])
+    .exec((err,result)=>{
+        if (err) {
+            res.status(400).send(err.message);
+        } else {    
+            res.send(result)
+        }
+    })
+}
+
+exports.showTopTenSchools = (req,res)=>{
+    var result_type = req.body.result_type 
+    Student.aggregate([
+       {
+           $project: {
+            _id : 0,   
+            name: 1,
+            school: 1,
+            'total_marks': {
+              $reduce: {
+                input: "$subjects",
+                initialValue: 0,
+                in: {
+                    $sum: [
+                        "$$value",
+                        {
+                            $cond:{
+                                if: {$eq: [ result_type, "yearly" ] },
+                                then: "$$this.yearly",
+                                else: "$$this.quartely"
+                            }
+                        }
+                        
+                     ]
+                }
+              }
+            }
+          }
+       },
+       {
+           $group: {
+                _id : "$school",
+                'total_students_marks' : {
+                    $sum : "$total_marks"
+                },
+                'total_students': {
+                    $sum : 1
+                }
+           }
+       },
+       {
+           $project: {
+                "avg_points": {
+                    $divide: [ "$total_students_marks", "$total_students" ]
+                } 
+           }
+       },
+       {
+            $sort : {avg_points: -1}
+       },
+       {
+            $limit : 10  
+       } 
+    ])
+    .exec((err,result)=>{
+        if (err) {
+            res.status(400).send(err.message);
+        } else {    
+            res.send(result)
+        }
+    })
+}
+
+exports.showTopTenStudentsInSubjectAcrossAllSchools = (req,res)=>{
+    var selected_subject = req.body.subject
+    var result_type = req.body.result_type 
+    Student.aggregate([
+       {
+           $unwind: "$subjects"
+       },
+       {
+           $match : { "subjects.subject": selected_subject }
+       },
+       {
+           $project:{
+               _id : 0,
+               name: 1,
+               school: 1,
+                'score': {
+                    $cond:{
+                        if : {$eq : [result_type, "yearly" ] },
+                        then : "$subjects.yearly" ,
+                        else : "$subjects.quartely"
+                    }
+                }
+           }
+       },
+       {
+           $sort: { score: -1}
+       }
+
+    ])
+    .exec((err,result)=>{
+        if (err) {
+            res.status(400).send(err.message);
+        } else {    
+            res.send(result)
+        }
+    })
+}
+
+exports.showTopTenStudentsInSubjectAcrossSchool = (req,res)=>{
+    var selected_school = req.body.school
+    var selected_subject = req.body.subject
+    var result_type = req.body.result_type 
+    Student.aggregate([
+        {
+            $match : {school: selected_school}
+        },
+       {
+           $unwind: "$subjects"
+       },
+       {
+           $match : { "subjects.subject": selected_subject }
+       },
+       {
+           $project:{
+               _id : 0,
+               name: 1,
+               school: 1,
+                'score': {
+                    $cond:{
+                        if : {$eq : [result_type, "yearly" ] },
+                        then : "$subjects.yearly" ,
+                        else : "$subjects.quartely"
+                    }
+                }
+           }
+       },
+       {
+           $sort: { score: -1}
+       }
+
     ])
     .exec((err,result)=>{
         if (err) {
--- routes/student_routes.js
+++ routes/student_routes.js
@@ -5,5 +5,7 @@ const student_controller = require("../controllers/student_controller");
 router.post("/add-student",student_controller.addStudent);
 router.get("/top-ten/all",student_controller.showTopTenStudentsAcrossAllSchools);
 router.get("/top-ten/school",student_controller.showTopTenStudentsAcrossSchool);
-
+router.get("/top-ten/schools",student_controller.showTopTenSchools);
+router.get("/top-ten/subject/all",student_controller.showTopTenStudentsInSubjectAcrossAllSchools);
+router.get("/top-ten/subject/school",student_controller.showTopTenStudentsInSubjectAcrossSchool);
 module.exports = router;
