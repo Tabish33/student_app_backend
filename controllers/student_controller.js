bf9e622323489f992e015f0fa9fa18223f88166d
Tabish Bashir <tabishbashir@Tabishs-MacBook-Pro.local>
2020-05-04 20:50:08 +0530

    implement showTopTenSchoolsInSubject

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -175,6 +175,66 @@ exports.showTopTenSchools = (req,res)=>{
     })
 }
 
+exports.showTopTenSchoolsInSubject = (req,res)=>{
+    var selected_subject = req.body.subject
+    var result_type = req.body.result_type 
+    Student.aggregate([
+       {
+           $unwind : "$subjects"
+       },
+       {
+           $match : {"subjects.subject": selected_subject}
+       }, 
+       {
+           $project: {
+            _id : 0,   
+            name: 1,
+            school: 1,
+            'total_marks': {
+                $sum: {
+                    $cond:{
+                        if: {$eq: [ result_type, "yearly" ] },
+                        then: "$subjects.yearly",
+                        else: "$subjects.quartely"
+                    }
+                }
+                }
+            }
+       },
+       {
+           $group: {
+                _id : "$school",
+                'total_students_marks' : {
+                    $sum : "$total_marks"
+                },
+                'total_students': {
+                    $sum : 1
+                }
+           }
+       },
+       {
+           $project: {
+                "avg_points": {
+                    $divide: [ "$total_students_marks", "$total_students" ]
+                } 
+           }
+       },
+       {
+            $sort : {avg_points: -1}
+       },
+       {
+            $limit : 10  
+       } 
+    ])
+    .exec((err,result)=>{
+        if (err) {
+            res.status(400).send(err.message);
+        } else {    
+            res.send(result)
+        }
+    })
+}
+
 exports.showTopTenStudentsInSubjectAcrossAllSchools = (req,res)=>{
     var selected_subject = req.body.subject
     var result_type = req.body.result_type 
--- routes/student_routes.js
+++ routes/student_routes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const student_controller = require("../controllers/student_controller");
 
 router.post("/add-student",student_controller.addStudent);
-router.get("/top-ten/all",student_controller.showTopTenStudentsAcrossAllSchools);
-router.get("/top-ten/school",student_controller.showTopTenStudentsAcrossSchool);
-router.get("/top-ten/schools",student_controller.showTopTenSchools);
+router.get("/top-ten/students/all",student_controller.showTopTenStudentsAcrossAllSchools);
+router.get("/top-ten/students/school",student_controller.showTopTenStudentsAcrossSchool);
+router.get("/top-ten/schools/all",student_controller.showTopTenSchools);
+router.get("/top-ten/schools/subject",student_controller.showTopTenSchoolsInSubject);
 router.get("/top-ten/subject/all",student_controller.showTopTenStudentsInSubjectAcrossAllSchools);
 router.get("/top-ten/subject/school",student_controller.showTopTenStudentsInSubjectAcrossSchool);
 module.exports = router;
