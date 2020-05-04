e0a9ff2507f05527516f9cc4eaf0a54dfe17f9e4
Tabish Bashir <tabishbashir@Tabishs-MacBook-Pro.local>
2020-05-04 14:35:07 +0530

    connect to db set up directory structure

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const student_routes = require("./routes/student_routes")
+const PORT = process.env.PORT || 3000
+app.use(express.json())
+
+app.use('/student',student_routes);
+
+
+//mongoDB connection string
+var uri = "mongodb://tabish:connection@cluster0-shard-00-00-n8mua.mongodb.net:27017,cluster0-shard-00-01-n8mua.mongodb.net:27017,cluster0-shard-00-02-n8mua.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+// CONNECT TO MONGODB
+mongoose.connect(uri,{useNewUrlParser:true})
+.then(()=>{
+    console.log("database connected");
+})
+.catch(err=>{
+    console.log(err);
+})
+
+app.listen(PORT,()=>{
+    console.log("Listening on port",PORT); 
+})
+
 
